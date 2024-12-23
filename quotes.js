239a9557ac29f6e46fac93a06946c2c02a654457
Arcanumsbc <arcanumsbc@yandex.ru>
2024-12-24 01:57:49 +0300

    Реализия добавления цитат в понравившиеся внизу и добавил кнопку добавления и удаления цитат из понравившихся

--- quotes.js
+++ quotes.js
@@ -2,42 +2,52 @@ const quotes = [
   {
     quote: 'The only way to do great work is to love what you do.',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: 'Innovation distinguishes between a leader and a follower.',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: 'Your time is limited, so don\'t waste it living someone else\'s life.',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: 'Success is not final, failure is not fatal: It is the courage to continue that counts.',
     author: 'Winston Churchill',
+    isFavorite: false,
   },
   {
     quote: 'Be yourself; everyone else is already taken.',
     author: 'Oscar Wilde',
+    isFavorite: false,
   },
   {
     quote: 'In the middle of difficulty lies opportunity.',
     author: 'Albert Einstein',
+    isFavorite: false,
   },
   {
     quote: 'The best way to predict the future is to invent it.',
     author: 'Alan Kay',
+    isFavorite: false,
   },
   {
     quote: 'I have not failed. I\'ve just found 10,000 ways that won\'t work.',
     author: 'Thomas Edison',
+    isFavorite: false,
   },
   {
     quote: 'It does not matter how slowly you go as long as you do not stop.',
     author: 'Confucius',
+    isFavorite: false,
   },
   {
     quote: 'The journey of a thousand miles begins with one step.',
     author: 'Lao Tzu',
+    isFavorite: false,
   },
 ];
 
