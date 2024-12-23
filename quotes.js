a4ec071a15974e949b0ec160a2eb16c0eb1e93cc
Arcanumsbc <arcanumsbc@yandex.ru>
2024-12-24 00:34:12 +0300

    Разбил приложение на JS модули

---
+++ quotes.js
@@ -0,0 +1,44 @@
+const quotes = [
+  {
+    quote: 'The only way to do great work is to love what you do.',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Innovation distinguishes between a leader and a follower.',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Your time is limited, so don\'t waste it living someone else\'s life.',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Success is not final, failure is not fatal: It is the courage to continue that counts.',
+    author: 'Winston Churchill',
+  },
+  {
+    quote: 'Be yourself; everyone else is already taken.',
+    author: 'Oscar Wilde',
+  },
+  {
+    quote: 'In the middle of difficulty lies opportunity.',
+    author: 'Albert Einstein',
+  },
+  {
+    quote: 'The best way to predict the future is to invent it.',
+    author: 'Alan Kay',
+  },
+  {
+    quote: 'I have not failed. I\'ve just found 10,000 ways that won\'t work.',
+    author: 'Thomas Edison',
+  },
+  {
+    quote: 'It does not matter how slowly you go as long as you do not stop.',
+    author: 'Confucius',
+  },
+  {
+    quote: 'The journey of a thousand miles begins with one step.',
+    author: 'Lao Tzu',
+  },
+];
+
+export default quotes;
