981030ab22d00e826f0d410487252036e67f29e4
Arcanumsbc <arcanumsbc@yandex.ru>
2024-12-25 01:54:07 +0300

    Рефакторинг - создание модулей избранной и цитаты

---
+++ src/data/quotes.js
@@ -0,0 +1,55 @@
+const quotes = [
+  {
+    text: 'The only way to do great work is to love what you do.',
+    author: 'Steve Jobs',
+    isFavorite: false,
+  },
+  {
+    text: 'Innovation distinguishes between a leader and a follower.',
+    author: 'Steve Jobs',
+    isFavorite: false,
+  },
+  {
+    text: 'Your time is limited, so don\'t waste it living someone else\'s life.',
+    author: 'Steve Jobs',
+    isFavorite: false,
+  },
+  {
+    text: 'Success is not final, failure is not fatal: It is the courage to' +
+      ' continue that counts.',
+    author: 'Winston Churchill',
+    isFavorite: false,
+  },
+  {
+    text: 'Be yourself; everyone else is already taken.',
+    author: 'Oscar Wilde',
+    isFavorite: false,
+  },
+  {
+    text: 'In the middle of difficulty lies opportunity.',
+    author: 'Albert Einstein',
+    isFavorite: false,
+  },
+  {
+    text: 'The best way to predict the future is to invent it.',
+    author: 'Alan Kay',
+    isFavorite: false,
+  },
+  {
+    text: 'I have not failed. I\'ve just found 10,000 ways that won\'t work.',
+    author: 'Thomas Edison',
+    isFavorite: false,
+  },
+  {
+    text: 'It does not matter how slowly you go as long as you do not stop.',
+    author: 'Confucius',
+    isFavorite: false,
+  },
+  {
+    text: 'The journey of a thousand miles begins with one step.',
+    author: 'Lao Tzu',
+    isFavorite: false,
+  },
+];
+
+export default quotes;
